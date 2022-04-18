f0fbca1b3eaec5bcc47e3452bba363bfa235c247
valeriistefanyk <valeriistefanyk@users.noreply.github.com>
2022-04-18 18:30:01 +0000

    deploy: 79b6e1888529e1302ce40e8e8eaa9e508cfb3535

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "16d6ee60a05206f6aa27281610557a4f",
-"index.html": "48cf8d27a6914ab2a10f886ce4ebf504",
-"/": "48cf8d27a6914ab2a10f886ce4ebf504",
+  "main.dart.js": "302cec0a6bfe2b193002322e3dc7db9d",
+"index.html": "d2947969330150d7ed24cc3059168712",
+"/": "d2947969330150d7ed24cc3059168712",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/AssetManifest.json": "7b0dde0e6a57cea782980f924c165049",
@@ -271,7 +271,7 @@ const RESOURCES = {
 "assets/packages/libphonenumber_plugin/assets/js/stringbuffer.js": "5d8ac81dc841740b2a94e8daa7be8027",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/assets/data/orders.json": "4eba4a8639062815cd0d3e8c4a0fa312",
-"assets/assets/data/tariffs.json": "cfbec65674712db2ac7a5c8d0da629da",
+"assets/assets/data/tariffs.json": "b5830fcc968643ab1c8e539952cdb88a",
 "assets/assets/data/lockers.json": "79dab527d63a884d3139082494743548",
 "assets/assets/images/welcome_img.png": "589cda18885e67f28ddd8272df633b09",
 "assets/assets/images/uploads/bakery_box.png": "b00b23e0467e71ffec1c7bc293f2840e",
