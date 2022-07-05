6584527a148fad95db606c66f758e9b9fb2eb453
valeriistefanyk <valeriistefanyk@users.noreply.github.com>
2022-07-05 11:14:37 +0000

    deploy: a17689641f9433296cd514cff03a310611f3f9e0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -8,8 +8,8 @@ const RESOURCES = {
 "canvaskit/profiling/canvaskit.js": "ae2949af4efc61d28a4a80fffa1db900",
 "canvaskit/profiling/canvaskit.wasm": "95e736ab31147d1b2c7b25f11d4c32cd",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
-"index.html": "3ff4a110023cf191f5155b58cee32d24",
-"/": "3ff4a110023cf191f5155b58cee32d24",
+"index.html": "254067fb75d812e271903437e43153d8",
+"/": "254067fb75d812e271903437e43153d8",
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/Icon-maskable-192.png": "3b04175c6495f4788d10c50f623fabec",
 "icons/Icon-maskable-512.png": "42bab81f1e23cd1ce49a011399731f03",
@@ -298,7 +298,7 @@ const RESOURCES = {
 "assets/packages/intl_phone_number_input/assets/flags/mp.png": "87351c30a529071ee9a4bb67765fea4f",
 "assets/packages/intl_phone_number_input/assets/flags/mc.png": "90c2ad7f144d73d4650cbea9dd621275",
 "assets/NOTICES": "03dd26b018edcf493a88648104c1ec32",
-"main.dart.js": "25bffd8c96360df584c071af0824abac",
+"main.dart.js": "8cee5c29e75bbf405192e134f75ca31a",
 "main.js": "84980ad582cdcc5a03873f98da896ff3"
 };
 
